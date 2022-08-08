ac3268c769d857245cd6858632b872def490729e
Kiaan Moodley <2202459@students.wits.ac.za>
2022-08-08 14:24:50 +0200

    Combat Scripts added

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+    [SerializeField] GameObject ragdoll;
+
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        animator.SetTrigger("damage");
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(ragdoll, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+}
